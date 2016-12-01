683592e5fb0b3a6690f04a7d4e2cf06dac159218
Krid Jin <jinklub.krid@bit.edu.cn>
2016-12-01 13:23:30 +0800

    Update server.js
    change port listener

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/testPost',function(req, res){
   res.json({"msg":"Posted requested"});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
   console.log('avaliable services: testGet,testGetWithParam,testPost,testPostWithParam');
 });
